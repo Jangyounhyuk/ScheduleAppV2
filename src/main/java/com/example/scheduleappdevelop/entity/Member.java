d5c646e2acc527b4120be1d8fe7366e37c9a9e6e
JangYounhyuk <hyuk905@gmail.com>
2025-02-11 23:14:06 +0900

    ãentity 코드 일부 ì 수정

--- src/main/java/com/example/scheduleappdevelop/entity/Member.java
+++ src/main/java/com/example/scheduleappdevelop/entity/Member.java
@@ -1,15 +1,13 @@
 package com.example.scheduleappdevelop.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @NoArgsConstructor
+@Table(name = "member")
 public class Member extends BaseEntity{
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/scheduleappdevelop/entity/Schedule.java
+++ src/main/java/com/example/scheduleappdevelop/entity/Schedule.java
@@ -1,10 +1,7 @@
 package com.example.scheduleappdevelop.entity;
 
 import com.example.scheduleappdevelop.dto.response.ScheduleResponseDto;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +10,7 @@ import java.time.LocalDateTime;
 @Getter
 @Entity
 @NoArgsConstructor
+@Table(name = "schedule")
 public class Schedule extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
